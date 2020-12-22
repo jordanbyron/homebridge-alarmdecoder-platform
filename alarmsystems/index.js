54a83199cf0e07aeb9bbafbd9e2f7410a71d7352
Jordan Byron <jordan@jordanbyron.com>
2020-12-22 11:14:19 -0500

    Add custom redis based alarm system

--- alarmsystems/index.js
+++ alarmsystems/index.js
@@ -1,4 +1,5 @@
 module.exports.HoneywellDSC = require('./honeywell.js').HoneywellDSC;
 module.exports.Interlogix = require('./interlogix.js').Interlogix;
+module.exports.CustomRedisAlarm = require('./custom-redis-alarm.js').CustomRedisAlarm;
 module.exports.AlarmZone = require('./base.js').AlarmZone;
 module.exports.AlarmBase = require('./base.js').AlarmBase;
